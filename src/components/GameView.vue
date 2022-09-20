6d5d18f5bdc208a26e6abe83db48a13db5fd3ea7
sakura-0 <1471979213@qq.com>
2022-09-20 20:08:01 +0800

    pref: create

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -19,37 +19,57 @@ const ctx = ref<fabric.Canvas>()
 // }
 
 function create() {
-  if (canvasDom.value) {
-    // ctx.value = canvasDom.value.getContext('2d') || undefined
-    // ratio.value = getPixelRatio(ctx.value)
-    // canvasDom.value.width = 750 * ratio.value
-    // canvasDom.value.height = canvasDom.value.height * ratio.value
-    // animation()
-    ctx.value = new fabric.Canvas('canvas')
-    animation()
-  }
+  ctx.value = new fabric.Canvas('canvas')
+
+  ctx.value!.selection = false // 不允许直接从画布框选
+
+  animation()
+  // }
+}
+function createStyle(el: any) {
+  el.perPixelTargetFind = true
+  // 禁止水平移动
+  el.lockMovementX = true
+  el.lockMovementY = true
+  // 禁止旋转
+  el.lockRotation = true
+  // 禁止水平缩放
+  el.lockScalingX = true
+  // 禁止水平缩放
+  el.lockScalingY = true
+  // 没有边框
+  el.hasBorders = false
+  // 没有控制角
+  el.hasControls = false
 }
 // 开启动画
 function animation() {
   // 创建一个长方形
-  const rect = new fabric.Rect({
-    top: 30, // 距离容器顶部 30px
+  const rect = new fabric.Circle({
+    top: 200,
+    radius: 80, // 距离容器顶部 30px
     left: 30, // 距离容器左侧 30px
-    width: 2000, // 宽 100px
-    height: 1000, // 高 60px
     fill: 'red', // 填充 红色
+  })
+  createStyle(rect)
+  rect.on('selected', (op) => {
+    // console.log(op)
+    // console.log('点解了', op)
   })
   ctx.value?.add(rect)
   // requestAnimationFrame(animation)
 }
 
 onMounted(() => {
-  create()
+  nextTick(() => {
+    create()
+  })
 })
 </script>
 
 <template>
   <div ref="fDom" w="100%" h="100%">
-    <canvas id="canvas" ref="canvasDom" :width="fDom?.clientWidth" :height="fDom?.clientHeight">浏览器不支持</canvas>
+    <canvas v-if="fDom" id="canvas" ref="canvasDom" style="border: 1px solid #ccc;" :width="fDom?.clientWidth" :height="fDom?.clientHeight">浏览器不支持</canvas>
+    <!-- <canvas id="canvas" width="400" height="400" style="border: 1px solid #ccc;" /> -->
   </div>
 </template>
